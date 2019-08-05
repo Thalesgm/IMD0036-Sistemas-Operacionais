8081efef6ac98db3774efe4eeda0a42f9a484503
Thales Gomes Moreira <thalesgm@A101-20L.imd.local>
2019-08-05 20:48:20 -0300

    Correcting bugs

--- Atividade01.py
+++ Atividade01.py
@@ -3,6 +3,7 @@
 #Discente:Thales Moreira
 #Mat: 2014026290
 import os
+import array
 
 def child():
 	#print('\nA new child %d' % os.getpid())
@@ -13,7 +14,7 @@ def insert_ids(a, count):
 	a.append(os.getpid())
 	i = 1;
 	while i <= count:
-		a.append(os.fork)
+		a.append(os.fork())
 		if a[i] == 0:
 			child()
 		i += 1
@@ -31,27 +32,27 @@ def print_status(a):
 def kill_cp(a, id):
 	if id == a[0]:
 		print("ID corresponds to PARENT ID. To kill parent ID, use the Kill Parent(P) Option.") 
-    else:
-    	i == 0
-    	while i < len(a):
-    		if id = a[i]
-    			os.kill(a[i], 9)
-        		a.pop(id)
-        		break
-        	i += 1
-        if i == len(a):
-        	print("Invalid Process ID")
+	else:
+		if i == 0:
+			while i < len(a):
+				if id == a[i]:
+					os.kill(a[i], 9)
+					a.pop(id)
+					break
+			i += 1
+		if i == len(a):
+			print("Invalid Process ID")
 
 def program():
 	pids = array.array('i')  
-	insert_ids(pids)
+	insert_ids(pids, 4)
 	while True:
 		print_status(pids)
 		reply = input("How would you like to proceed:\nK-Kill a Process\nQ-Quit\n")
 		if reply == 'K' or 'k':
 			option = input("C-Kill a Child Process?\nP-Kill Parent?(Suicide)\n")
 			if option == 'P'or 'p':
-				confirm = input("Are you sure you want to Kill Parent Process(Y,N\n")
+				confirm = input("Are you sure you want to Kill Parent Process(Y,N)\n")
 				if confirm == 'Y' or 'y':
 					os.kill(os.getpid(), 9)
 				else:
